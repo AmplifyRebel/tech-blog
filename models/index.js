39759f3eddcb7959cbc9994807376c8cb91d9cbe
Amplify Rebel <erj2204@gmail.com>
2022-05-10 12:42:27 -0500

    models

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+Post.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+module.exports = {
+  User,
+  Comment,
+  Post
+};
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: "cascade"
+})
