ff5474ea913ad8388fbac418772dec4cac9029e8
Amplify Rebel <erj2204@gmail.com>
2022-05-11 23:21:15 -0500

    changed routes

--- controller/api/comment-routes.js
+++ controller/api/comment-routes.js
@@ -1,7 +1,7 @@
 
 const router = require("express").Router();
 const { Comment } = require("../../models/");
-const withAuth = require("../../utils/auth");
+const withAuth = require("../../utils/authorize");
 
 router.post("/", withAuth, (req, res) => {
   Comment.create({ ...req.body, userId: req.session.userId })
